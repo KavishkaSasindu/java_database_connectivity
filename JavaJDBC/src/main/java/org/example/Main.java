6e9ad4a034fbd68e52aab77933de8eb7c5a3e5ed
KavishkaSasindu <sasindu0215@gmail.com>
2025-02-04 13:47:26 +0530

    update the user field

--- JavaJDBC/src/main/java/org/example/Main.java
+++ JavaJDBC/src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -19,7 +17,7 @@ public class Main {
 
         String url = "jdbc:postgresql://localhost:5432/users";
         String username = "postgres";
-        String password = "000000";
+        String password = "";
 
 
         //load the driver
@@ -29,6 +27,25 @@ public class Main {
         Connection conn = DriverManager.getConnection(url,username,password);
 
         System.out.println(conn);
+
+
+//        create statement
+        String QUERY = "select fname from person where personid = 1";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(QUERY);
+        rs.next(); //  it is go first row.it is mandatory
+
+//        process and result
+        User user = new User();
+
+        String firstName = rs.getString("fname");
+        user.setFirstname(firstName);
+
+        System.out.println(firstName);
+
+        System.out.println(rs.next());
+
+
 
     }
 }
